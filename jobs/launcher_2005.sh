9279419251e4644a2c99bd517e389a631dc37266
Michelle Audirac <audiracmichelle@michelles-air.lan>
2021-11-19 08:03:34 -0600

    added jobs for 2005

---
+++ jobs/launcher_2005.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J crosswalk_grid_zip            # job name
+#SBATCH -N 2                        # number of nodes requested
+#SBATCH -n 60                       # total number of tasks to run in parallel
+#SBATCH -p skx-dev                  # queue (partition) 
+#SBATCH -t 00:30:00                 # run time (hh:mm:ss) 
+
+module load tacc-singularity
+module load launcher
+
+export LD_PRELOAD=""
+export LAUNCHER_WORKDIR=/work/08317/m1ch3ll3/stampede2/pm25_grid_zip
+export LAUNCHER_JOB_FILE=jobs/jobs_2005
+
+${LAUNCHER_DIR}/paramrun
